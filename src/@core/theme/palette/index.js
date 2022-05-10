c14a4c204582012a9a439338b288f7e2ab795d33
sachin-Encodus <sachin1245e@gmail.com>
2022-05-10 17:49:09 +0530

    dark mode

--- src/@core/theme/palette/index.js
+++ src/@core/theme/palette/index.js
@@ -90,8 +90,8 @@ const DefaultPalette = (mode, themeColor) => {
     },
     divider: `rgba(${mainColor}, 0.12)`,
     background: {
-      paper: mode === 'light' ? '#FFF' : '#312D4B',
-      default: mode === 'light' ? '#F4F5FA' : '#28243D'
+      paper: mode === 'light' ? '#FFF' : '#050505',
+      default: mode === 'light' ? '#F4F5FA' : '#18181a'
     },
     action: {
       active: `rgba(${mainColor}, 0.54)`,
